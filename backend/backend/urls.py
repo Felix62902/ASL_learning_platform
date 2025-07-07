498896f2dd936591c674a7b0f4c596963e0df540
Felix62902 <xfelixwongx@gmail.com>
2025-07-07 13:05:29 +0100

    initial backend setup

---
+++ backend/backend/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from api.views import CreateUserView
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/user/register/", CreateUserView.as_view(), name="register"),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/", include("api.urls")),
+]
